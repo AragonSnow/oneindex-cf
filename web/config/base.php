da21e3ecfefa35c7305b0ba76de204900a31cc98
AragonSnow <asd159874123@126.com>
2019-11-02 21:23:35 +0800

    v1.1 add coonfig file

---
+++ web/config/base.php
@@ -0,0 +1,106 @@
+<?php return array (
+  'site_name' => 'Aragon\'s OneIndex',
+  'password' => 'Qwe159874123',
+  'style' => 'material',
+  'onedrive_root' => '/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'stND93|#-mlftuSMSZA834%',
+  'client_id' => '70a135da-f61c-491d-b04d-0f8d94da2ee0',
+  'redirect_uri' => 'https://oneindex.github.io/',
+  'refresh_token' => '',
+  'onedrive_hide' => '',
+  'onedrive_hotlink' => '',
+);
---
+++ web/config/token.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'token_type' => 'Bearer',
+  'scope' => 'Files.ReadWrite.All profile openid email',
+  'expires_in' => 3600,
+  'ext_expires_in' => 3600,
+  'access_token' => '',
+  'refresh_token' => '',
+  'expires_on' => 1566659090,
+);
